46f8a2c6c412dfe65a13e368f142edefb712a8d7
christophejacques <cjacques.programmes@numericable.fr>
2020-07-13 00:24:11 +0200

    maj

--- utils/ma_liste.py
+++ utils/ma_liste.py
@@ -1,9 +1,9 @@
 
 class ma_liste:
     
-    def __init__(self):
+    def __init__(self, liste=[]):
         
-        self.liste = []
+        self.liste = liste
         self.idx_max = -1
         self.debut = 0
     
@@ -48,7 +48,7 @@ class ma_liste:
         self.idx_max += 1
 
     def clear(self):
-        self.__init__()
+        self.__init__([])
         
     def copy(self):
         return self.liste.copy()
@@ -56,8 +56,35 @@ class ma_liste:
     def count(self):
         return self.idx_max+1
         
+    def extend(self, autre_liste):
+        if type(autre_liste) == type([]):
+            self.liste.extend(autre_liste)
+            self.idx_max += len(autre_liste)
+        else:
+            raise TypeError(f"Le paramètre {autre_liste} n'est pas de type liste")
+            
     def index(self, valeur):
         return self.liste.index(valeur)
+        
+    def insert(self, index, valeur):
+        if index <= self.idx_max and index >= 0:
+            self.liste.insert(index, valeur)
+        
+    def pop(self, numero=None):
+        if numero is None:
+            res = self.liste.pop(self.idx_max)
+        else:
+            res = self.liste.pop(numero)
+        
+        self.idx_max -= 1
+        return res
+        
+    def remove(self, valeur):
+        self.liste.remove(valeur)
+        
+    def reverse(self):
+        self.liste.reverse()
+        
 
 try:
     l = ma_liste()
@@ -67,7 +94,7 @@ try:
     l.append("deux")
     l.append("trois")
     l.append("quatre")
-    print(l.index("deuxs"))
+    print(l.index("deux"))
     
     print(l)
     l[0] = "zero"
@@ -76,11 +103,18 @@ try:
     print()
     del l[1]
     print(l)
+    
+    print("\nclear")
     l.clear()
+    print(l)
+    
+    l.extend([9,5,1])
+    print(l)
+    print("Remove :", l.pop())
     print(l)
     
 except Exception as e:
     print("Error:", e)
     
 from msvcrt import getch
-getch()
+#getch()
